4de502f6b9f710437e046bb86c2d4dfc96e14c92
EnnaLeuark <Akrauel@posteo.de>
2022-01-22 17:21:46 +0100

    updates CSS for sunrise, sunset images full screen

--- src/Sunrise.js
+++ src/Sunrise.js
@@ -4,15 +4,21 @@ import "./Sunrise.css";
 export default function Sunrise() {
   return (
     <>
-      <div className="sunrise">
-        <img src="images/sunrise.svg" className="sunIcon" alt="sunrise icon" />
-        <br />
-        <span id="sunriseTime">05:43</span>
-      </div>
-      <div className="sunset">
-        <img src="images/sunrise.svg" className="sunIcon" alt="sunset icon" />
-        <br />
-        <span id="sunsetTime">21:51</span>
+      <div className="sunIcons">
+        <div className="sunrise">
+          <img
+            src="images/sunrise.svg"
+            className="sunIcon"
+            alt="sunrise icon"
+          />
+          <br />
+          <span id="sunriseTime">05:43</span>
+        </div>
+        <div className="sunset">
+          <img src="images/sunrise.svg" className="sunIcon" alt="sunset icon" />
+          <br />
+          <span id="sunsetTime">21:51</span>
+        </div>
       </div>
     </>
   );
